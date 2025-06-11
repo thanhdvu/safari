007879a8dba997dd4874f242f8c370a89e938e17
thanhdvu <thuyanhdv@gmail.com>
2025-06-11 21:02:34 +0900

    add docstring

--- world.py
+++ world.py
@@ -2,12 +2,17 @@ import random
 
 class World:
     def __init__(self, width: int = 50, height: int = 50):
+        """
+        지도의 크기를 설정하고, 동물 리스트를 초기화합니다.
+        """
         self.width = width
         self.height = height
         self.animals = []  # list of animals
 
     def initialize(self, create_zebra, create_lion):
-        # make a list of all positions
+        """
+        얼룩말 20마리, 사자 5마리를 무작위 위치에 배치합니다.
+        """
         positions = []
         for x in range(self.width):
             for y in range(self.height):
@@ -15,17 +20,18 @@ class World:
 
         random.shuffle(positions)
 
-        # place 20 zebras
         for _ in range(20):
             x, y = positions.pop()
             self.animals.append(create_zebra(x, y))
 
-        # place 5 lions
         for _ in range(5):
             x, y = positions.pop()
             self.animals.append(create_lion(x, y))
 
     def display(self):
+        """
+        . = 빈칸, 0 = 얼룩말, X = 사자
+        """
         for y in range(self.height):
             row = ""
             for x in range(self.width):
